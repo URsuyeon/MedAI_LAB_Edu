726a7f7e6753f0dbbc06c316f761b58448b677eb
suyeon <suyeon040102@gmail.com>
2025-03-07 16:35:39 +0900

    기본 속성 확인

--- Part2_Registration/P2_0.py
+++ Part2_Registration/P2_0.py
@@ -4,3 +4,14 @@
 #   Image.numpy() method 이용하여 numpy 배열 얻어보기 + 시각화 해보기
 #   Image.plot(axis=N) 이용하여 시각화해보고 위랑 비교
 #   ants.image_read() 함수로 nifti 실습파일 아무거나 읽고 위 과정 반복
+
+import ants
+
+dicom_path = "/mnt/c/Users/SU/Desktop/materials/3d/dicom/S.D PRE-CONTRAST"
+dicom_image = ants.dicom_read(dicom_path)
+
+print(dicom_image.spacing)
+print(dicom_image.shape)
+print(dicom_image.dtype)
+print(dicom_image.dimension)
+
